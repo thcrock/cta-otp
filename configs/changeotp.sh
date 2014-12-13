c76c7188545d6d9e388bd8f400129b355b50ea0e
Tristan Crockett <tristan@edgeflip.com>
2014-12-13 15:01:56 -0600

    Tomcat 7 compatibility

--- configs/changeotp.sh
+++ configs/changeotp.sh
@@ -4,26 +4,26 @@
 # Must be run as root
 
 # Stop the server
-service tomcat6 stop
+service tomcat7 stop
 
 # Remove old wars/folders
-rm /usr/share/tomcat6/webapps/opentripplanner-analyst-client.war
-rm /usr/share/tomcat6/webapps/opentripplanner-api-webapp.war
-rm /usr/share/tomcat6/webapps/opentripplanner-ga-client.war
-rm /usr/share/tomcat6/webapps/opentripplanner-webapp.war
-rm -r -f /usr/share/tomcat6/webapps/opentripplanner-analyst-client
-rm -r -f /usr/share/tomcat6/webapps/opentripplanner-api-webapp
-rm -r -f /usr/share/tomcat6/webapps/opentripplanner-ga-client
-rm -r -f /usr/share/tomcat6/webapps/opentripplanner-webapp
+rm /usr/share/tomcat7/webapps/opentripplanner-analyst-client.war
+rm /usr/share/tomcat7/webapps/opentripplanner-api-webapp.war
+rm /usr/share/tomcat7/webapps/opentripplanner-ga-client.war
+rm /usr/share/tomcat7/webapps/opentripplanner-webapp.war
+rm -r -f /usr/share/tomcat7/webapps/opentripplanner-analyst-client
+rm -r -f /usr/share/tomcat7/webapps/opentripplanner-api-webapp
+rm -r -f /usr/share/tomcat7/webapps/opentripplanner-ga-client
+rm -r -f /usr/share/tomcat7/webapps/opentripplanner-webapp
 
 #Add new wars/folders
-cp ../opentripplanner-analyst-client/target/opentripplanner-analyst-client.war /usr/share/tomcat6/webapps
-cp ../opentripplanner-api-webapp/target/opentripplanner-api-webapp.war /usr/share/tomcat6/webapps
-cp ../opentripplanner-ga-client/target/opentripplanner-ga-client.war /usr/share/tomcat6/webapps
-cp ../opentripplanner-webapp/target/opentripplanner-webapp.war /usr/share/tomcat6/webapps 
+cp ../opentripplanner-analyst-client/target/opentripplanner-analyst-client.war /usr/share/tomcat7/webapps
+cp ../opentripplanner-api-webapp/target/opentripplanner-api-webapp.war /usr/share/tomcat7/webapps
+cp ../opentripplanner-ga-client/target/opentripplanner-ga-client.war /usr/share/tomcat7/webapps
+cp ../opentripplanner-webapp/target/opentripplanner-webapp.war /usr/share/tomcat7/webapps 
 
 # Start the server again
-service tomcat6 start
+service tomcat7 start
 
 # Exit
 exit
