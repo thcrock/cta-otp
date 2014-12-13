a8f577f1cfe39d0b36632e927dd187edea9a1b35
Tristan Crockett <tristan@edgeflip.com>
2014-12-13 16:43:33 -0600

    More tomcat7 updates

--- configs/changeotp.sh
+++ configs/changeotp.sh
@@ -7,20 +7,20 @@
 service tomcat7 stop
 
 # Remove old wars/folders
-rm /usr/share/tomcat7/webapps/opentripplanner-analyst-client.war
-rm /usr/share/tomcat7/webapps/opentripplanner-api-webapp.war
-rm /usr/share/tomcat7/webapps/opentripplanner-ga-client.war
-rm /usr/share/tomcat7/webapps/opentripplanner-webapp.war
-rm -r -f /usr/share/tomcat7/webapps/opentripplanner-analyst-client
-rm -r -f /usr/share/tomcat7/webapps/opentripplanner-api-webapp
-rm -r -f /usr/share/tomcat7/webapps/opentripplanner-ga-client
-rm -r -f /usr/share/tomcat7/webapps/opentripplanner-webapp
+rm /var/lib/tomcat7/webapps/opentripplanner-analyst-client.war
+rm /var/lib/tomcat7/webapps/opentripplanner-api-webapp.war
+rm /var/lib/tomcat7/webapps/opentripplanner-ga-client.war
+rm /var/lib/tomcat7/webapps/opentripplanner-webapp.war
+rm -r -f /var/lib/tomcat7/webapps/opentripplanner-analyst-client
+rm -r -f /var/lib/tomcat7/webapps/opentripplanner-api-webapp
+rm -r -f /var/lib/tomcat7/webapps/opentripplanner-ga-client
+rm -r -f /var/lib/tomcat7/webapps/opentripplanner-webapp
 
 #Add new wars/folders
-cp ../opentripplanner-analyst-client/target/opentripplanner-analyst-client.war /usr/share/tomcat7/webapps
-cp ../opentripplanner-api-webapp/target/opentripplanner-api-webapp.war /usr/share/tomcat7/webapps
-cp ../opentripplanner-ga-client/target/opentripplanner-ga-client.war /usr/share/tomcat7/webapps
-cp ../opentripplanner-webapp/target/opentripplanner-webapp.war /usr/share/tomcat7/webapps 
+cp ../opentripplanner-analyst-client/target/opentripplanner-analyst-client.war /var/lib/tomcat7/webapps
+cp ../opentripplanner-api-webapp/target/opentripplanner-api-webapp.war /var/lib/tomcat7/webapps
+cp ../opentripplanner-ga-client/target/opentripplanner-ga-client.war /var/lib/tomcat7/webapps
+cp ../opentripplanner-webapp/target/opentripplanner-webapp.war /var/lib/tomcat7/webapps 
 
 # Start the server again
 service tomcat7 start
